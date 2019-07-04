59153157aad594e63890c04638dab9067fd73105
RyotaMoriyama <info@ryotamoriyama.com>
2019-07-04 14:51:25 +0900

    Modify rename and theme merge process

--- script/wordpress.js
+++ script/wordpress.js
@@ -29,10 +29,11 @@ inquirer.prompt([{
         .then(function () {
             preparing.start();
             childProcess.spawnSync('sed', ["-i", "", "-e", "s|WP_THEME_NAME=lig|WP_THEME_NAME=" + answer.name + "|g", path.join(process.cwd(), "docker.env")]);
-            childProcess.spawnSync('sed', ["-i", "", "-e", "s|!/wp/wp-content/themes/input-theme-name/|!/wp/wp-content/themes/" + answer.name + "/|g", path.join(process.cwd(), ".gitignore")]);
+            childProcess.spawnSync('sed', ["-i", "", "-e", "s|!/wp/wp-content/themes/lig/|!/wp/wp-content/themes/" + answer.name + "/|g", path.join(process.cwd(), ".gitignore")]);
             childProcess.spawnSync('sed', ["-i", "", "-e", "s|'input-theme-name'|'" + answer.name + "'|g", path.join(process.cwd(), "webpack.mix.js")]);
+            childProcess.spawnSync('mv', ["wp/wp-content/themes/input-theme-name/inc", "wp/wp-content/themes/lig/"]);
             fs.renameSync(path.join(process.cwd(), 'resources/themes/input-theme-name'), path.join(process.cwd(), 'resources/themes', answer.name));
-            fs.renameSync(path.join(process.cwd(), 'wp/wp-content/themes/input-theme-name'), path.join(process.cwd(), 'wp/wp-content/themes', answer.name));
+            fs.renameSync(path.join(process.cwd(), 'wp/wp-content/themes/lig'), path.join(process.cwd(), 'wp/wp-content/themes', answer.name));
             preparing.succeed();
         });
 });

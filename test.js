f3801e17eab6ead27d0ffb2ae01a6fb134deb709
RyotaMoriyama <info@ryotamoriyama.com>
2021-03-13 00:43:03 +0900

    Remove test

--- test.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-const promise = require('promise');
-const childProcess = require('child_process');
-const wpLatestVersion = childProcess.spawnSync('git', ["ls-remote", "--tags", "--refs", "--sort='v:refname'", "https://github.com/WordPress/WordPress", "|", "tail", "-n1", "|","sed", "'s/.*\\///'"],{
-    shell: true
-});

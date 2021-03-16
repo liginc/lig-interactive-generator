48920729c81833168606bb96fd63125c11605cf3
RyotaMoriyama <info@ryotamoriyama.com>
2021-03-16 10:27:57 +0900

    Modify installing pkg

--- script/wordpress.js
+++ script/wordpress.js
@@ -40,66 +40,66 @@ inquirer.prompt([{
 }]).then((answer) => {
     const thePromise = Promise.resolve();
     thePromise
-        .then(() => {
-            download('https://github.com/liginc/lig-docker-wordpress.git', {
-                mergeEnvSample: true
-            });
-        })
+        // .then(() => {
+        //     download('https://github.com/liginc/lig-docker-wordpress.git', {
+        //         mergeEnvSample: true
+        //     });
+        // })
         .then(() => {
             download('https://github.com/liginc/laravel-mix-boilerplate-wordpress.git', {
                 mergeEnvSample: true
             })
         })
-        .then(() => {
-            if (answer.wordpress_type == 'with theme') {
-                fs.removeSync(path.join(rootDir, "resources"))
-                fs.removeSync(path.join(rootDir, "wp/wp-content/themes/input-theme-name"))
-                download('https://github.com/liginc/lig-wordpress-template.git').then(() => {
-                    fs.renameSync(path.join(rootDir, 'wp/wp-content/themes/lig-wordpress-template'), path.join(rootDir, 'wp/wp-content/themes', projectName));
-                    fs.renameSync(path.join(rootDir, 'resources/themes/lig-wordpress-template'), path.join(rootDir, 'resources/themes', projectName));
-                }).then(()=>{
-                    download('https://github.com/liginc/lig-wordpress-functions.git', {
-                        destDir: 'wp/wp-content/themes/' + projectName,
-                    });
-                });
-            } else {
-                fs.renameSync(path.join(rootDir, 'wp/wp-content/themes/input-theme-name'), path.join(rootDir, 'wp/wp-content/themes', projectName));
-                fs.renameSync(path.join(rootDir, 'resources/themes/input-theme-name'), path.join(rootDir, 'resources/themes', projectName));
-                download('https://github.com/liginc/lig-wordpress-functions.git', {
-                    destDir: 'wp/wp-content/themes/' + projectName,
-                });
-            }
-        })
-        .then(() => {
-            preparing.start();
-
-            // Replace text on .env
-            if (answer.php_ver !== '') childProcess.spawnSync('sed', ["-i", "", "-e", "s|PHP_VER=.*$|PHP_VER=" + answer.php_ver + "|g", env]);
-            if (answer.mysql_ver !== '') childProcess.spawnSync('sed', ["-i", "", "-e", "s|MYSQL_VER=.*$|MYSQL_VER=" + answer.mysql_ver + "|g", env]);
-            if (answer.wordpress_ver !== '') childProcess.spawnSync('sed', ["-i", "", "-e", "s|WP_VERSION=.*$|WP_VERSION=" + answer.wordpress_ver + "|g", env]);
-
-            childProcess.spawnSync('sed', ["-i", "", "-e", "s|WP_THEME_NAME=.*$|WP_THEME_NAME=" + projectName + "|g", env]);
-            childProcess.spawnSync('sed', ["-i", "", "-e", "s|input-theme-name|" + projectName + "|g", env]);
-            childProcess.spawnSync('sed', ["-i", "", "-e", "s|wordpress.test|localhost|g", env]);
-
-            fs.readFile(webpackMixJs, 'utf8', function (err,data) {
-                if (err) {
-                    return console.log(err);
-                }
-                data = data.replace(/(const srcRelativePath =).+?\((.+?)\s.+?.replace\(.+?\)/s, '$1 $2;');
-                data = data.replace(/(const distRelativePath =).+?\((.+?)\s.+?.replace\(.+?\)/s, '$1 $2;');
-                fs.writeFile(webpackMixJs, data, 'utf8', function (err) {
-                    if (err) return console.log(err);
-                });
-            });
-
-            preparing.succeed();
-        })
-        .then(() => {
-            let pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'))
-            pkg.engineStrict = true
-            pkg.engines.node = nodeVersion
-            fs.writeFileSync(pkgPath, JSON.stringify(pkg,null,2));
-            process.stdout.write("Set node version into package.json \n")
-        })
+        // .then(() => {
+        //     if (answer.wordpress_type == 'with theme') {
+        //         fs.removeSync(path.join(rootDir, "resources"))
+        //         fs.removeSync(path.join(rootDir, "wp/wp-content/themes/input-theme-name"))
+        //         download('https://github.com/liginc/lig-wordpress-template.git').then(() => {
+        //             fs.renameSync(path.join(rootDir, 'wp/wp-content/themes/lig-wordpress-template'), path.join(rootDir, 'wp/wp-content/themes', projectName));
+        //             fs.renameSync(path.join(rootDir, 'resources/themes/lig-wordpress-template'), path.join(rootDir, 'resources/themes', projectName));
+        //         }).then(()=>{
+        //             download('https://github.com/liginc/lig-wordpress-functions.git', {
+        //                 destDir: 'wp/wp-content/themes/' + projectName,
+        //             });
+        //         });
+        //     } else {
+        //         fs.renameSync(path.join(rootDir, 'wp/wp-content/themes/input-theme-name'), path.join(rootDir, 'wp/wp-content/themes', projectName));
+        //         fs.renameSync(path.join(rootDir, 'resources/themes/input-theme-name'), path.join(rootDir, 'resources/themes', projectName));
+        //         download('https://github.com/liginc/lig-wordpress-functions.git', {
+        //             destDir: 'wp/wp-content/themes/' + projectName,
+        //         });
+        //     }
+        // })
+        // .then(() => {
+        //     preparing.start();
+        //
+        //     // Replace text on .env
+        //     if (answer.php_ver !== '') childProcess.spawnSync('sed', ["-i", "", "-e", "s|PHP_VER=.*$|PHP_VER=" + answer.php_ver + "|g", env]);
+        //     if (answer.mysql_ver !== '') childProcess.spawnSync('sed', ["-i", "", "-e", "s|MYSQL_VER=.*$|MYSQL_VER=" + answer.mysql_ver + "|g", env]);
+        //     if (answer.wordpress_ver !== '') childProcess.spawnSync('sed', ["-i", "", "-e", "s|WP_VERSION=.*$|WP_VERSION=" + answer.wordpress_ver + "|g", env]);
+        //
+        //     childProcess.spawnSync('sed', ["-i", "", "-e", "s|WP_THEME_NAME=.*$|WP_THEME_NAME=" + projectName + "|g", env]);
+        //     childProcess.spawnSync('sed', ["-i", "", "-e", "s|input-theme-name|" + projectName + "|g", env]);
+        //     childProcess.spawnSync('sed', ["-i", "", "-e", "s|wordpress.test|localhost|g", env]);
+        //
+        //     fs.readFile(webpackMixJs, 'utf8', function (err,data) {
+        //         if (err) {
+        //             return console.log(err);
+        //         }
+        //         data = data.replace(/(const srcRelativePath =).+?\((.+?)\s.+?.replace\(.+?\)/s, '$1 $2;');
+        //         data = data.replace(/(const distRelativePath =).+?\((.+?)\s.+?.replace\(.+?\)/s, '$1 $2;');
+        //         fs.writeFile(webpackMixJs, data, 'utf8', function (err) {
+        //             if (err) return console.log(err);
+        //         });
+        //     });
+        //
+        //     preparing.succeed();
+        // })
+        // .then(() => {
+        //     let pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'))
+        //     pkg.engineStrict = true
+        //     pkg.engines.node = nodeVersion
+        //     fs.writeFileSync(pkgPath, JSON.stringify(pkg,null,2));
+        //     process.stdout.write("Set node version into package.json \n")
+        // })
 });
--- util/husky.js
+++ util/husky.js
@@ -1,28 +1,48 @@
 'use strict';
 const fs = require('fs-extra');
 const path = require('path');
-const childProcess = require('child_process');
 const ora = require('ora');
 
 function setHusky(rootDir, scriptPath) {
+
+    // husky用のdevDependenciesをpackage.jsonに追加
     const pkgPath = path.join(rootDir, 'package.json')
-    const huskyLibPath = path.join(scriptPath, 'lib/husky')
-    ora(`npm ci`).start()
-    childProcess.execSync('cd '+rootDir+' && npm ci')
-    ora(`npm ci`).succeed()
-    ora(`Installing husky packages`).start()
-    childProcess.execSync('cd '+rootDir+' && npm i husky lint-staged prettier eslint-config-prettier --save-dev')
-    ora(`Installing husky packages`).succeed()
-    ora(`Update package.json`).start()
     let pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'))
+    const huskyLibPath = path.join(scriptPath, 'lib/husky')
+    const pkgHuskyPath = path.join(huskyLibPath, 'package.json')
+    const pkgHusky = JSON.parse(fs.readFileSync(pkgHuskyPath, 'utf8'))
+    for (let k of Object.keys(pkgHusky.devDependencies)) {
+        if (!(k in pkg.devDependencies)) {
+            pkg.devDependencies[k] = pkgHusky.devDependencies[k]
+        }
+    }
+
+    // scriptsを追加
     pkg.scripts.prepare = "husky install"
     pkg.scripts.test = "lint-staged -c .lintstagedrc.js"
-    fs.writeFileSync(pkgPath, JSON.stringify(pkg,null,2))
+    fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2))
     ora(`Update package.json`).succeed()
+
+    ora(`Update package-lock.json`).start()
+    const pkgLockPath = path.join(rootDir, 'package-lock.json')
+    let pkgLock = JSON.parse(fs.readFileSync(pkgLockPath, 'utf8'))
+    const pkgLockHuskyPath = path.join(huskyLibPath, 'package-lock.json')
+    const pkgLockHusky = JSON.parse(fs.readFileSync(pkgLockHuskyPath, 'utf8'))
+    for (let k of Object.keys(pkgLockHusky.dependencies)) {
+        // package-lock.jsonはバージョン比較をする
+        if (!(k in pkgLock.dependencies) || pkgLock.dependencies[k]['version'] < pkgLockHusky.dependencies[k]['version']) {
+            pkgLock.dependencies[k] = pkgLockHusky.dependencies[k]
+        }
+    }
+    fs.writeFileSync(pkgLockPath, JSON.stringify(pkgLock, null, 2))
+    ora(`Update package-lock.json`).succeed()
+
     ora('Copying husky setting files').start()
     const files = fs.readdirSync(huskyLibPath)
     files.forEach(file => {
-        fs.copyFileSync(path.join(huskyLibPath, file), path.join(rootDir, file))
+        if (file != "package.json" && file != "package-lock.json") {
+            fs.copyFileSync(path.join(huskyLibPath, file), path.join(rootDir, file))
+        }
     })
     ora('Copying husky setting files').succeed()
 }
